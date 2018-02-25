c44dec48d61e3225201814a9e7bd3f412c91c682
Vishal2706 <30524261+Vishal2706@users.noreply.github.com>
2018-02-25 12:17:36 +0530

    Primary Data Structure

---
+++ data_Structures/projectfront.h
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+frontpage()
+{
+printf("\n\n");
+printf("                                                     DATA STRUCTURE AND ALGORITHM \n");
+printf("                                                              PROJECT\n\n\n");
+system("color 3e");
+printf("                              *********************************************************************************\n\n");
+printf("                                                 DEPARTMENT OF COMPUTER SCIENCE AND ENGINEERING  \n  ");
+printf("                                             Dr. B R AMBEDKAR NATIONAL INSTITUTE OF TECHNOLOGY\n");
+printf("                                                   JALANDHAR – 144011, PUNJAB (INDIA)\n");
+printf("                                                         JULY-DECEMBER, 2016 \n\n");
+printf("                              **********************************************************************************\n\n");
+printf("                                                         CONTENTS :-\n");
+printf("                                              1- Linked list\n");
+printf("                                              2- Queue\n");
+printf("                                              3- Stack\n");
+printf("                                              4- Application of stack\n");
+printf("                                              5- Tree\n");
+printf("                                              6- Sorting techiques\n");
+printf("                                              7- Searching techniqes\n\n\n\n\n\n\n\n");
+printf("  SUBMITTED TO :                                                                          SUBMITTED BY :\n");
+printf("  Mr. Ashish Kumar                                                                        Name - Vishal Gautam\n");
+printf("  Assistant Professor                                                                     Roll Number- 15103047\n");
+printf("  Dept. of CSE                                                                            Department - CSE\n\n\n");
+printf("                                             Let's Start project\n");
+}
